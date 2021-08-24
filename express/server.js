4768981536d1fda1d4c897cae2557eec6586fe8e
Daniiba <danielibanezbecker@aol.com>
2021-08-24 17:00:03 +0200

    proxy

--- express/server.js
+++ express/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request')
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -13,7 +14,35 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/medium', (req, res) => { 
 
+  request(
+    { url: 'https://medium.com/feed/@vabble' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message:
+      err.message })
+    }
+    
+    res.set({'Content-Type': 'application/rss+xml'})
+    res.send(Buffer.from(body))
+  })
+})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
